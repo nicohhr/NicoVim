9b2a894090a6e5121ff37c3836b7a63d29bc4eee
Nicolas Abraham <niko.hr@proton.me>
2026-08-11 16:23:36 -0400

    Fix Docker Compose LSP attachment

--- init.lua
+++ init.lua
@@ -1,2 +1,22 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.filetype.add({
+  filename = {
+    ["compose.yaml"] = "yaml.docker-compose",
+    ["compose.yml"] = "yaml.docker-compose",
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["docker-compose.yml"] = "yaml.docker-compose",
+  },
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[bufnr].filetype == "yaml.docker-compose" then
+        vim.api.nvim_exec_autocmds("FileType", { buffer = bufnr })
+      end
+    end
+  end,
+})
+
 require("config.lazy")
